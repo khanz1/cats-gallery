3cc8fe7d764b841dd7f324766e3ab5a29938953b
khanz1 <assistance.xavier@gmail.com>
2024-07-27 04:26:25 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,84 @@
+# Cat Gallery
+
+Welcome to **Cat Gallery** - a simple and beautiful gallery application showcasing various cat images. This project is built using Next.js, TypeScript, and Tailwind CSS. This repository is intended for a workshop focused on deploying client-side applications.
+
+![Cat Gallery Screenshot](./public/cats-gallery.png)
+
+## Table of Contents
+
+- [Features](#features)
+- [Getting Started](#getting-started)
+  - [Prerequisites](#prerequisites)
+  - [Installation](#installation)
+  - [Running the Project](#running-the-project)
+- [Deployment](#deployment)
+- [Technologies Used](#technologies-used)
+
+## Features
+
+- Display a beautiful gallery of cat images.
+- TypeScript for type safety and improved developer experience.
+
+## Getting Started
+
+### Prerequisites
+
+Ensure you have the following installed on your machine:
+
+- Node.js (>= 18.17)
+- npm or yarn
+
+### Installation
+
+Clone the repository:
+
+git clone https://github.com/khanz1/cats-gallery.git
+cd cat-gallery
+
+Install the dependencies:
+
+Using npm:
+
+```
+npm install
+```
+
+Or using yarn:
+
+```
+yarn install
+```
+
+### Running the Project
+
+Start the development server:
+
+Using npm:
+
+```
+npm run dev
+```
+
+Or using yarn:
+
+```
+yarn dev
+```
+
+Open your browser and navigate to `http://localhost:5173`.
+
+## Deployment
+
+This project will be deployed on [Jagoan Hosting](https://www.jagoanhosting.com/)
+
+For deployment instructions, follow these steps:
+
+```
+step to deploy
+```
+
+## Technologies Used
+
+- **Next.js**: A React framework for server-side rendering and static site generation.
+- **TypeScript**: A superset of JavaScript that adds static types.
+- **Tailwind CSS**: A utility-first CSS framework for rapid UI development.
---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import {useEffect, useState} from 'react'
+import InfiniteScroll from "react-infinite-scroll-component";
+
+function App() {
+    const [cats, setCats] = useState([]);
+
+    const fetchData = async () => {
+        const url = new URL('https://api.thecatapi.com');
+        url.pathname = '/v1/images/search';
+        url.searchParams.set('limit', "25");
+
+        const response = await fetch(url.toString());
+        const data = await response.json()
+
+        setCats(prevCats => {
+            return prevCats.concat(data);
+        })
+    }
+    useEffect(() => {
+        void fetchData();
+    }, []);
+
+  return (
+      <main className="flex min-h-screen flex-col items-center">
+          <h1 className="text-xl sm:text-2xl lg:text-4xl font-bold py-5">Welcome to the Cat Gallery</h1>
+          <InfiniteScroll
+              dataLength={cats.length}
+              next={fetchData}
+              hasMore={true}
+              loader={
+                  <p className="text-center my-5">
+                      <b>Loading...</b>
+                  </p>}
+              endMessage={
+                  <p className="text-center my-5">
+                      <b>Yay! You have seen it all</b>
+                  </p>
+              }
+          >
+              <div className="grid grid-cols-12 gap-3 w-3/4 m-auto">
+                  {cats.map((cat) => (
+                      <div key={cat.id}
+                           className="lg:col-span-3 sm:col-span-6 col-span-12 hover:filter hover:cursor-pointer grayscale hover:filter-none">
+                          <div className="rounded-lg overflow-hidden shadow-lg" style={{height: 200}}>
+                              <img src={cat.url} alt="Cat" className="w-full h-full object-cover"/>
+                          </div>
+                      </div>
+                  ))}
+              </div>
+          </InfiniteScroll>
+      </main>
+  )
+}
+
+export default App
